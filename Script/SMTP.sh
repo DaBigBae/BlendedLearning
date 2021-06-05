7b595506b242170c5dfafc436004b74590793e83
BeBuNNL <nl98hk@gmail.com>
2021-06-05 16:16:22 +0700

    update sed command SMTP

--- Script/SMTP.sh
+++ Script/SMTP.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
-USER=${1}
-PASS=${2}
+MAIL=''
+PASS=''
 SERVER="smtp.gmail.com"
 PORT=587
 TLS="true"
 
 show_help(){
     echo "
-Usage: SMTP.sh [EMAILADDRESS] [PASSWORD] [OPTION]\nMain options:
+Usage: SMTP.sh [-e EMAILADDRESS] [-p PASSWORD] [OPTION]
+Main options:
+    -e Your service email you want to setup
+    -p Your email password
     -s Email server (Default is smtp.gmail.com)
-    -p Port email host service (Default is 587)
+    -o Port email host service (Default is 587)
     -t Don't use TLS when transmit email
-For example: ./SMTP.sh test@gmail.com yourpass\nHelp option:
+For example: ./SMTP.sh test@gmail.com yourpass
+Help option:
     -h  Help
     "
+    exit
 }
 
-OPTIND=3
-while getopts :s:pt:h flag
+while getopts :e:p:s:ot:h flag
 do
     case "$flag" in
+        e) #Email
+            MAIL=$OPTARG
+        ;;
+        p) #Password
+            PASS=$OPTARG
+        ;;
         s) #Email server
             SERVER=$OPTARG
         ;;
-        p) #Email service port
+        o) #Email service port
             PORT=$OPTARG
         ;;
         t) #Use TLS when transmit email
             TLS="false"
+        ;;
         h) #help
             show_help
         ;;
@@ -39,21 +50,21 @@ do
     esac
 done
 
-sed -i "s/"EMAIL_HOST_PASSWORD"/"EMAIL_HOST_PASSWORD": "$PASS",/" /edx/etc/lms.yml
-sed -i "s/"EMAIL_HOST_USER"/"EMAIL_HOST_USER": "$USER",/" /edx/etc/lms.yml
+sed -i -e "s/.*EMAIL_HOST_PASSWORD.*/EMAIL_HOST_PASSWORD: $PASS/" /edx/etc/lms.yml
+sed -i -e "s/.*EMAIL_HOST_USER.*/EMAIL_HOST_USER: $MAIL/" /edx/etc/lms.yml
 
-sed -i "s/"EMAIL_HOST_PASSWORD"/"EMAIL_HOST_PASSWORD": "$PASS",/" /edx/etc/studio.yml
-sed -i "s/"EMAIL_HOST_USER"/"EMAIL_HOST_USER": "$USER",/" /edx/etc/studio.yml
+sed -i -e "s/.*EMAIL_HOST_PASSWORD.*/EMAIL_HOST_PASSWORD: $PASS/" /edx/etc/studio.yml
+sed -i -e "s/.*EMAIL_HOST_USER.*/EMAIL_HOST_USER: $MAIL/" /edx/etc/studio.yml
 
-sed -i "s/"EMAIL_BACKEND"/"EMAIL_BACKEND": "django.core.mail.backends.smtp.EmailBackend",/" /edx/etc/lms.yml
-sed -i "s/"EMAIL_HOST"/"EMAIL_HOST": "$SERVER",/" /edx/etc/lms.yml
-sed -i "s/"EMAIL_PORT"/"EMAIL_PORT": "$PORT",/" /edx/etc/lms.yml
-sed -i "s/"EMAIL_USE_TLS"/"EMAIL_USE_TLS": "$TLS",/" /edx/etc/lms.yml
+sed -i -e "s/.*EMAIL_BACKEND.*/EMAIL_BACKEND: django.core.mail.backends.smtp.EmailBackend/" /edx/etc/lms.yml
+sed -i -e "s/.*EMAIL_HOST.*/EMAIL_HOST: $SERVER/" /edx/etc/lms.yml
+sed -i -e "s/.*EMAIL_PORT.*/EMAIL_PORT: $PORT/" /edx/etc/lms.yml
+sed -i -e "s/.*EMAIL_USE_TLS.*/EMAIL_USE_TLS: $TLS/" /edx/etc/lms.yml
 
-sed -i "s/"EMAIL_BACKEND"/"EMAIL_BACKEND": "django.core.mail.backends.smtp.EmailBackend",/" /edx/etc/studio.yml
-sed -i "s/"EMAIL_HOST"/"EMAIL_HOST": "$SERVER",/" /edx/etc/studio.yml
-sed -i "s/"EMAIL_PORT"/"EMAIL_PORT": "$PORT",/" /edx/etc/studio.yml
-sed -i "s/"EMAIL_USE_TLS"/"EMAIL_USE_TLS": "$TLS",/" /edx/etc/studio.yml
+sed -i -e "s/.*EMAIL_BACKEND.*/EMAIL_BACKEND: django.core.mail.backends.smtp.EmailBackend/" /edx/etc/studio.yml
+sed -i -e "s/.*EMAIL_HOST.*/EMAIL_HOST: $SERVER/" /edx/etc/studio.yml
+sed -i -e "s/.*EMAIL_PORT.*/EMAIL_PORT: $PORT/" /edx/etc/studio.yml
+sed -i -e "s/.*EMAIL_USE_TLS.*/EMAIL_USE_TLS: $TLS/" /edx/etc/studio.yml
 
 /edx/bin/supervisorctl restart lms
 /edx/bin/supervisorctl restart cms
