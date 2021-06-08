62dfa7447d4d6108c0ce0e39fcea09a982bac59e
BeBu <nl98hk@gmail.com>
2021-06-08 23:10:50 +0700

    update hướng dẫn

--- Script/compileStaticAssets.sh
+++ Script/compileStaticAssets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # cp logo png to default theme /edx/app/edxapp/edx-platform/lms/static/images
-# sed -i "s/.*ENABLE_COMPREHENSIVE_THEMING\W.*/"ENABLE_COMPREHENSIVE_THEMING": true," /edx/etc/lms.yml
+sed -i "s/.*ENABLE_COMPREHENSIVE_THEMING\W.*/ENABLE_COMPREHENSIVE_THEMING: true/" /edx/etc/lms.yml
 
 sudo -H -u edxapp bash << EOF
 source /edx/app/edxapp/edxapp_env
